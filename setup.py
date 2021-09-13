d2626ac6389f0d3aa6e9213009691a4ff3c7db7d
SEEUN Dev <47962021+entry0917@users.noreply.github.com>
2021-09-13 20:36:01 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='urltool',
+
+version='1.1.0A',
+
+description='A simple url shortener that made with python.',
+
+author='Seeun Kim',
+
+author_email='seeun0917@naver.com',
+
+url='https://dev.kimseeun.site',
+
+license='MIT',
+
+py_modules=['urltool'],
+
+python_requires='>=3',
+
+install_requires=[],
+
+packages=['urltool']
+
+)
