b406b8503d72bdf9e3173ec79cbd391265c8ab6b
SEEUN Dev <47962021+entry0917@users.noreply.github.com>
2021-09-14 14:45:56 +0900

    Create setup.b.py

---
+++ setup.b.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='urltool',
+
+version='1.1.0',
+
+description='A simple url_tools that made with python.',
+
+author='Seeun Kim',
+
+author_email='seeun0917@naver.com',
+
+url='https://github.com/seeundev/urltool',
+
+license='MIT',
+
+py_modules=['urltool'],
+
+python_requires='>=3',
+
+install_requires=[],
+
+packages=['urltool']
+
+)
+# Here is beta urltool setup.py.
